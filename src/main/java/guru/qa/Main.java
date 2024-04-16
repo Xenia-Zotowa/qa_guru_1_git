d822c581d4d127885a8c784aae6962f2cbbf8688
Xenia Zotova <ksenias.zotova@gmail.com>
2024-04-17 01:48:56 +0300

    changed

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,15 @@
 package guru.qa;
 
 public class Main {
+
+    private String msg = "Hello qa guru!";
+
     public static void main(String[] args) {
-        System.out.println("Hello qa guru!");
+        System.out.println(getMassage());
     }
+
+    private static String getMassage() {
+        return msg;
+    }
+    
 }
