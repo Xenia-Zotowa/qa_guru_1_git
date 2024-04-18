387cf807b017faa7ce6e1220fc705a57d25c4c5b
Xenia Zotova <ksenias.zotova@gmail.com>
2024-04-18 16:12:39 +0300

    hallo massage cheng

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,7 +2,7 @@ package guru.qa;
 
 public class Main {
 
-    private String msg = "Hello qa guru!";
+    private String msg = "Hello qa guru!!!";
 
     public void main(String[] args) {
         System.out.println(getMassage());
